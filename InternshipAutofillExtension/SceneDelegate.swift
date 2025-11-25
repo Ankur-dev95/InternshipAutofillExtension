597dfb9a18e87bb8e7ec305a94367b529b5a58ad
Ankur-dev95 <machaankur259@gmail.com>
2025-11-24 21:56:21 -0500

    Initial Commit

---
+++ InternshipAutofillExtension/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  InternshipAutofillExtension
+//
+//  Created by Ankur on 11/24/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
